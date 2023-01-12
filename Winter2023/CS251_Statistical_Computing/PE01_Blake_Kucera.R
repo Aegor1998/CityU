c3f04cd2ec5e8247587ffaf66a197c900321d933
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-01-12 11:31:25 -0800

    Update PE01_Blake_Kucera.R
    fixed spacing

--- Winter2023/CS251_Statistical_Computing/PE01_Blake_Kucera.R
+++ Winter2023/CS251_Statistical_Computing/PE01_Blake_Kucera.R
@@ -19,31 +19,31 @@ v1 <- c(v[is.integer(v / 3] = TRUE ) | v[is.integer(v / 5] = TRUE ))
 v1 <- unique(v1) #removes duplication (test is this is needed)
 # Create the vecotr whoes values are greater than 15 and contained in vector v
 v2 <- c(v[v > 15])
-#Printing the Vectors
+# Printing the Vectors
 "Vector v: "  + v
 "Vector v1: " + v1
 "Vector v2: " + v2
 
 
-#Calculate the Mean of each vector
+# Calculate the Mean of each vector
 v_mean  <- mean(v)
 v1_mean <- mean(v1)
 v2_mean <- mean(v2)
-#Print the Mean of each Vector
+# Print the Mean of each Vector
 "Mean of Vector v: "  + v_mean
 "Mean of Vector v1: " + v1_mean
 "Mean of Vector v2: " + v2_mean
 
-#Calculate the Coefficient of Variation of each Vector
+# Calculate the Coefficient of Variation of each Vector
 v_cv  <- sd(v)/v_mean
 v1_cv <- sd(v1)/v1_mean
 v2_cv <- sd(v2)/v2_mean
-#Print the Coefficient of Variation of each Vector
+# Print the Coefficient of Variation of each Vector
 "Coefficient of Variation of Vector v: "  + v_cv
 "Coefficient of Variation of Vector v1: " + v1_cv
 "Coefficient of Variation of Vector v2: " + v2_cv
 
-#Create cv_all
+# Create cv_all
 cv_all <- c(v_cv, v1_cv, v2_cv)
-#Print the lowest Coefficient of Variation between all Vectors
+# Print the lowest Coefficient of Variation between all Vectors
 "The lowest Coefficient of Variation is " + min(cv_all)
