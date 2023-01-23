bd8c22b7644246c3c2e29f246a90af51d5548fb6
bkucera <aegor1998@gmail.com>
2023-01-22 20:44:38 -0800

    HOP02 update

---
+++ Winter2023/CS251_Statistical_Computing/HOP02_Blake_Kucera/WorkWithDFrame.R
@@ -0,0 +1,14 @@
+# Author: Blake Kucera
+
+states <- state.x77
+print("Section: Head")
+print(head(states))
+
+print("Section: Tail")
+print(tail(states))
+
+print("Section: Summary")
+print(summary(states))
+
+print("Section: STR")
+print(str(states))
---
+++ Winter2023/CS251_Statistical_Computing/HOP02_Blake_Kucera/createDataFrame.R
@@ -0,0 +1,1 @@
+# Author: Blake Kucera
