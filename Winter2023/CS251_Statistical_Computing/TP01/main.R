ba70ca6fde2cf67319a3fbc8041a0ff0fc6483ca
Aegor1998 <50035405+Aegor1998@users.noreply.github.com>
2023-02-18 01:15:41 -0800

    Update main.R
    Did 4 things
    1) Fixed setwd() issue
    2) created cleanData() to cut down on lines
    3) created fix_table_CON() to fix data formatting for the condition graphs
    4) Created the graphs for Goal 1

--- Winter2023/CS251_Statistical_Computing/TP01/main.R
+++ Winter2023/CS251_Statistical_Computing/TP01/main.R
@@ -10,11 +10,14 @@
 #install.packages("ggplot2")
 #install.packages('data.table')
 #install.packages('dplyr')
+#install.packages("tidyverse")
 
 # Loads needed libaries
 library('ggplot2')
 library('data.table')
 library('dplyr')
+library('scales')
+library('rstudioapi')
 
 #Functions
 find_max <- function(x) {
@@ -25,22 +28,47 @@ find_min <- function(x) {
   return (sort(table(x), decreasing = FALSE)[1])
 }
 
+cleanData <- function(x) {
+  x      <- x[!is.na(x)]
+  x      <- x[x != "UNKNOWN"]
+  return (x)
+}
+
+fix_table_CON <- function(dataFrame){
+  colnames(dataFrame)[1] <- "Number"
+  newDataFrame <- data.frame(Type   = c("", "", ""),
+                             Event  = c("", "", ""),
+                             Number = c(0, 0, 0))
+  rowNames <- rownames(dataFrame)
+  number  <- NULL
+  counter <- 1
+
+  for (i in rowNames) {
+    temp <- unlist(strsplit(i, split="\\."))
+
+    newDataFrame$Type[counter]   <- temp[1]
+    newDataFrame$Event[counter]  <- temp[2]
+    newDataFrame$Number[counter] <- dataFrame$Number[counter]
+    counter <- counter + 1
+  }
+
+  return(newDataFrame)
+}
 
 # This script expects that the .cvs file will be in the smae folder
 # CVS name Traffic_Crashes_-_Crashes.csv
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 FILE <- read.csv("Traffic_Crashes_-_Crashes.csv")
 # Vector for crash related factors
-road_Condition      <- FILE$ROADWAY_SURFACE_COND  # ROADWAY_SURFACE_COND
-road_Condition      <- road_Condition[!is.na(road_Condition)]
-road_Condition      <- road_Condition[road_Condition != "UNKNOWN"]
+road_Condition <- FILE$ROADWAY_SURFACE_COND  # ROADWAY_SURFACE_COND
+road_Condition <- cleanData(road_Condition)
 
-light_Conditions      <- FILE$LIGHTING_CONDITION    # LIGHTING_CONDITION
-light_Conditions      <- light_Conditions[!is.na(light_Conditions)]
-light_Conditions      <- light_Conditions[light_Conditions != "UNKNOWN"]
+light_Conditions <- FILE$LIGHTING_CONDITION    # LIGHTING_CONDITION
+light_Conditions <- cleanData(light_Conditions)
 
-weather_Conditions      <- FILE$WEATHER_CONDITION     # WEATHER_CONDITION
-weather_Conditions      <- weather_Conditions[!is.na(weather_Conditions)]
-weather_Conditions      <- weather_Conditions[weather_Conditions != "UNKNOWN"]
+weather_Conditions <- FILE$WEATHER_CONDITION     # WEATHER_CONDITION
+weather_Conditions <- cleanData(weather_Conditions)
 
 # Variable for Population of Seattle and Number of Crashes
 Number_Of_Crashes   <- 690763       # 01/2016 - 12/2022
@@ -49,11 +77,30 @@ Current_Population  <- 2746388      # Pulled from
                                     # https://www.census.gov/quickfacts/fact/table/chicagocityillinois/POP010220
                                     # on 02/15/2023 1948. Census from April 1st 2020
 # Vector for Number of Deaths
-list_Of_Fatalities <- fread("Traffic_Crashes_-_Crashes.csv", select = "INJURIES_FATAL") # INJURIES_FATAL
+list_Of_Fatalities <- FILE$INJURIES_FATAL # INJURIES_FATAL
+list_Of_Fatalities <- cleanData(list_Of_Fatalities)
 
 # Goal 1: Conditions
 MAX_Condition <- data.frame(c(road = find_max(road_Condition),
-                            light = find_max(light_Conditions),
-                            weather = find_max(weather_Conditions)))
-colnames(MAX_Condition)[1] ="# Of Events"
+                              light = find_max(light_Conditions),
+                              weather = find_max(weather_Conditions)))
+
+MIN_Condition <- data.frame(c(road = find_min(road_Condition),
+                              light = find_min(light_Conditions),
+                              weather = find_min(weather_Conditions)))
+
+MAX_Condition <- fix_table_CON(MAX_Condition)
+MIN_Condition <- fix_table_CON(MIN_Condition)
+
+#Making Graphs for goal 1
+ggplot(data = MAX_Condition, aes(x=Event, y=Number, fill=Event)) +
+  geom_bar(stat = 'identity') +
+  scale_y_continuous(labels = scales::comma) +
+  ggtitle("Most likely conditions")
+
+ggplot(data = MIN_Condition, aes(x=Event, y=Number, fill=Event)) +
+  geom_bar(stat = 'identity') +
+  scale_y_continuous(labels = scales::comma) +
+    theme(axis.text.x=element_blank()) +
+  ggtitle("Least likely conditions")
 
